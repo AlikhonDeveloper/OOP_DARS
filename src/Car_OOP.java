cbb4ee7fdbfe7304c2205aa24569af8486ded691
Alixon <alixonzayniddinov@gmail.com>
2023-12-07 10:12:23 +0500

    added Uchburchak

--- src/Car_OOP.java
+++ src/Car_OOP.java
@@ -1,9 +1,19 @@
 public class Car_OOP {
     public static void main(String[] args) {
+       //1- Car Topshiriq
         Car car =new Car();
         System.out.println("Rang  "+car.color);
         System.out.println("Narxi "+car.narx+" ming dollar");
         System.out.println("Rusumi "+car.module);
         System.out.println("Uzatish qutisi "+car.Uzatish);
+        System.out.println("-------------------------------------");
+
+        // 2- Uch burchak
+        Uchburchak uchburchak=new Uchburchak();
+        uchburchak.tomonbir(3);
+        uchburchak.tomonikki(4);
+        uchburchak.tomonuch(5);
+
+
     }
 }
---
+++ src/Uchburchak.java
@@ -0,0 +1,12 @@
+public class Uchburchak {
+    public void tomonbir(int a){
+        System.out.println("Uchburchakning tomoni "+a+ " ga teng ");
+    }
+    public void tomonikki(int b){
+        System.out.println("Uchburchakning tomoni "+b+ " ga teng ");
+    }
+    public void tomonuch(int c){
+        System.out.println("Uchburchakning tomoni "+c+ " ga teng ");
+    }
+
+}
