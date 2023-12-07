4d8d985bdc215deb5b7f141a4ff65aa47348ed3d
Alixon <alixonzayniddinov@gmail.com>
2023-12-07 09:58:45 +0500

    Car about

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,8 @@
 public class Car {
+    public String color="Qizil";
+    public String module="BMW";
+    public int narx=70;
+    public String Uzatish="Avtomat";
+
+
 }
---
+++ src/Car_OOP.java
@@ -0,0 +1,9 @@
+public class Car_OOP {
+    public static void main(String[] args) {
+        Car car =new Car();
+        System.out.println("Rang  "+car.color);
+        System.out.println("Narxi "+car.narx+" ming dollar");
+        System.out.println("Rusumi "+car.module);
+        System.out.println("Uzatish qutisi "+car.Uzatish);
+    }
+}
