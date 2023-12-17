028adbee032bdcb812daa9231a2dcebba2691e61
Alixon <alixonzayniddinov@gmail.com>
2023-12-17 20:24:23 +0500

    new MathFunctions

--- src/MathFunctions.java
+++ src/MathFunctions.java
@@ -1,28 +1,27 @@
 public class MathFunctions {
-    static double PI=3.14159265359;
-    static double e=2.71828182846;
 
-    public static double doira_yuzi(double r){
-        System.out.println("Doiramimg yuzi "+PI*Math.pow(r,2)+ " ga teng ");
-        return PI*Math.pow(r,2);
+    public double add(double number1,double number2){
+        return number1+number2;
     }
-    public static double e_daraja(double a){
-        System.out.println(" e ning "+a+ " chi darajasi  "+ Math.pow(e,a)+ " ga teng ");
-        return Math.pow(e,a);
-
-    }
-    public static double uchburchak_yuzi(double a,double b,double c){
-        double p=(a+b+c)/2;
-        double s=p*(p-a)*(p-b)*(p-c);
-        System.out.println("Uchburchakning yuzi "+Math.sqrt(s)+" ga teng");
-        return Math.sqrt(s);
-
+    public double sub(double number1,double number2){
+        return number1-number2;
     }
 
-    public static void main(String[] args) {
-        doira_yuzi(5);
-        e_daraja(4);
-        uchburchak_yuzi(5,6,7);
-
+    public double multiply(double number1,double number2){
+        return number1*number2;
     }
+    public double div(double number1,double number2){
+        return number1/number2;
+    }
+    public double abs(double number){
+        if(number<0)
+            return -1*number;
+        else return number;
+    }
+    public double pow(double number){
+        return number*number;
+    }
+
+
+
 }
